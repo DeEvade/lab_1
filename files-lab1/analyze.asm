e7e160ae47391f53cc92b51156c580528b5b5faf
Oscar Gertling <gertling@n182-p104.eduroam.kth.se>
2023-01-25 17:06:25 +0100

    Upg 1 done

--- files-lab1/analyze.asm
+++ files-lab1/analyze.asm
@@ -5,11 +5,18 @@
 	.text
 main:
 	li	$s0,0x30
+	addi $s2, $0, 3 	# saves 3 in $s2
 loop:
 	move	$a0,$s0		# copy from s0 to a0
 	
 	li	$v0,11		# syscall with v0 = 11 will print out
+	div $s0, $s2		# Divides $s0 by 3, saves remainder in hi
+	mfhi $s3		# Move From hi register, special OP.
+	bne $s3, $0, skip 	# If hi (remainder) == 0, branch to skip.  
 	syscall			# one byte from a0 to the Run I/O window
+	
+skip:
+	
 
 	addi	$s0,$s0,1	# what happens if the constant is changed?
 	
